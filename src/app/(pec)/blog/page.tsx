f67e90ba3f33df0910aacb8a82f7b84ff35e768d
xapis-21 <xapisofficial@gmail.com>
2025-06-19 16:08:30 +0300

    refactor: simplify blog page component by removing searchParams and enhancing data fetching for improved clarity

--- src/app/(pec)/blog/page.tsx
+++ src/app/(pec)/blog/page.tsx
@@ -1,23 +1,21 @@
-import { ALL_BLOG_POSTS_QUERY, type BlogPosts } from "@/features/blog/lib/queries";
+import {
+  ALL_BLOG_POSTS_QUERY,
+  type BlogPosts,
+} from "@/features/blog/lib/queries";
 import { BlogView } from "@/features/blog/ui/views/blog-view";
 import { sanityFetch } from "@/sanity/lib/live";
 import { Suspense } from "react";
 
-interface Props {
-  searchParams: Promise<{ category: string } | undefined>;
-}
 
-export default async function Page({ searchParams }: Props) {
-
-  const {data: blogPosts}: {data: BlogPosts} = await sanityFetch({
+export default async function Page() {
+  const { data: blogPosts }: { data: BlogPosts } = await sanityFetch({
     query: ALL_BLOG_POSTS_QUERY,
-  })
+  });
 
-  const searchParamsValues = await searchParams;
 
   return (
     <Suspense fallback={<div>Loading...</div>}>
-      <BlogView blogPosts={blogPosts} activeCategory={searchParamsValues?.category} />
+      <BlogView blogPosts={blogPosts} />
     </Suspense>
-  )
+  );
 }
