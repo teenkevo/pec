f7d226d514f3bdac2247f6883c999f2581792c48
Kevin Mugumya <luwembamugumya@gmail.com>
2025-06-25 04:01:58 +0200

    fix: enable smooth scrolling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
 }>) {
   return (
     <html
-      // style={{ scrollBehavior: "smooth" }}
+      style={{ scrollBehavior: "smooth" }}
       lang="en"
       suppressHydrationWarning
     >
