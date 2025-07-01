325a491120ea9c60f238b5790a90f7f05f17c24a
xapis-21 <xapisofficial@gmail.com>
2025-07-01 22:57:06 +0300

    refactor: adjust layout and spacing in LatestStories component

--- src/features/blog/ui/components/latest-stories.tsx
+++ src/features/blog/ui/components/latest-stories.tsx
@@ -8,10 +8,10 @@ interface Props {
 const LatestStories = ({ posts }: Props) => {
   return (
     <div
-      className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 lg:gap-12 py-16 px-4 md:px-14"
+      className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12 pt-16 px-4 md:px-14"
       id="latest-stories"
     >
-      <div className="col-span-2 lg:col-span-2 lg:row-span-2">
+      <div className="md:col-span-2 lg:col-span-2 lg:row-span-2">
         <BlogPostCard post={posts[0]} isFeatured={true} />
       </div>
       {posts.slice(1, 3).map((post) => (
