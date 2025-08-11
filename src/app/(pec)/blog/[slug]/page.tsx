86c707bd2d8cd9b092632a142e96f3798d606723
Kevin Mugumya <luwembamugumya@gmail.com>
2025-08-11 17:02:03 +0200

    feat: implement metadata generation for blog posts
    - Added generateMetadata function to fetch and return blog post-specific metadata, enhancing SEO.
    - Included title, description, and Open Graph data based on the blog post details.
    - Utilized urlFor to format the main image URL for optimal display.

--- src/app/(pec)/blog/[slug]/page.tsx
+++ src/app/(pec)/blog/[slug]/page.tsx
@@ -1,16 +1,46 @@
 import { LoadingSkeleton } from "@/components/loading-skeleton";
-import {
-  SINGLE_PROJECT_QUERY,
-  SINGLE_PROJECT_RESULT,
-} from "@/features/projects/lib/queries";
 import { BlogPostView } from "@/features/blog/ui/views/blog-post-view";
 import { sanityFetch } from "@/sanity/lib/live";
 import { notFound } from "next/navigation";
 import { Suspense } from "react";
 import { BLOG_POST_BY_SLUG_QUERY, BlogPost } from "@/features/blog/lib/queries";
+import { Metadata, ResolvingMetadata } from "next";
+import { urlFor } from "@/sanity/lib/image";
 
 interface Props {
   params: Promise<{ slug: string }>;
+}
+
+export async function generateMetadata(
+  { params }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  const { slug } = await params;
+
+  const {
+    data: post,
+  }: {
+    data: BlogPost;
+  } = await sanityFetch({
+    query: BLOG_POST_BY_SLUG_QUERY,
+    params: {
+      slug,
+    },
+  });
+
+  return {
+    title: post.title,
+    description: post.category,
+    openGraph: {
+      title: post.title,
+      description: post.category,
+      images: [
+        {
+          url: urlFor(post.image).format("webp").quality(50).url() || "",
+        },
+      ],
+    },
+  };
 }
 
 export default async function Page({ params }: Props) {
@@ -26,12 +56,10 @@ export default async function Page({ params }: Props) {
       slug,
     },
   });
-  console.log(post, slug)
 
   if (!post) {
     notFound();
   }
-
 
   return (
     <Suspense fallback={<LoadingSkeleton />}>
